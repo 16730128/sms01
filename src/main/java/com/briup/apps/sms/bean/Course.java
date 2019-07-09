7fef117da2ebff0806e8a79fb211150f639a09cd
徐晶晶 <393201544@qq.com>
2019-07-09 17:51:42 +0800

    [徐晶晶]加了一行注释

--- src/main/java/com/briup/apps/sms/bean/Course.java
+++ src/main/java/com/briup/apps/sms/bean/Course.java
@@ -1,5 +1,34 @@
 package com.briup.apps.sms.bean;
-
+/*
+ * 课程类*/
 public class Course {
-
+	private Long id;
+	private String name;
+	private double credit;
+	private Long userid;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	public Long getUserid() {
+		return userid;
+	}
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+	
 }
