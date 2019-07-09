9c9ec50c15c1f3ada8fe5dc8a4a4c242b5e3e08d
彭浩 <814779307@qq.com>
2019-07-09 19:24:54 +0800

    [彭浩]创建了一个新类

---
+++ src/main/java/com/briup/apps/sms/bean/Role.java
@@ -0,0 +1,19 @@
+package com.briup.apps.sms.bean;
+
+public class Role {
+	private Long id;
+	private String name;
+	private Long getId() {
+		return id;
+	}
+	private void setId(Long id) {
+		this.id = id;
+	}
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+}
