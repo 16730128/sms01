07a1d0a0ef9ba3b19372fc2538776b3ac5020ca2
朱浩宇 <1214546173@qq.com>
2019-07-09 19:54:39 +0800

    [朱浩宇]修改了一个类

--- src/main/java/com/briup/apps/sms/bean/college.java
+++ src/main/java/com/briup/apps/sms/bean/college.java
@@ -1,5 +1,32 @@
 package com.briup.apps.sms.bean;
 
 public class college {
-
+	private Long id;
+	private String name;
+	private String description;
+	private Long shool_id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getdescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public long shool_id() {
+		return shool_id;
+	}
+	public void setshool_id(Long shool_id) {
+		this.shool_id = shool_id;
+	}
 }
