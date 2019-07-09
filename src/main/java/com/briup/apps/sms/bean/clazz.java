5eb46b711ed0061b01e6e04dc94924c9917dc926
蒲耀辉 <1025948141@qq.com>
2019-07-09 18:35:24 +0800

    [蒲耀辉]新建的类

---
+++ src/main/java/com/briup/apps/sms/bean/clazz.java
@@ -0,0 +1,27 @@
+package com.briup.apps.sms.bean;
+
+public class clazz {
+	private Long id;
+	private String name;
+	private String begain_time;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBegain_time() {
+		return begain_time;
+	}
+	public void setBegain_time(String begain_time) {
+		this.begain_time = begain_time;
+	}
+	
+
+}
