211c7e24b8df970e146071def779dc3aed878168
范东宏 <774784360@qq.com>
2019-07-09 17:35:06 +0800

    [范东宏]新建类

--- src/main/java/com/briup/apps/sms/bean/User.java
+++ src/main/java/com/briup/apps/sms/bean/User.java
@@ -1,35 +1,36 @@
 package com.briup.apps.sms.bean;
-
+/*
+ * 用户类*/
 public class User {
-	private Longint id;
-	private varchar realname;
-	private varchar telephone;
-	private varchar password;
+	private Long id;
+	private String realname;
+	private String telephone;
+	private String password;
 	private int student_no;
-	private varchar gender;
-	private bigint clazz_id;
-	public Longint getId() {
+	private String gender;
+	private Long clazz_id;
+	public Long getId() {
 		return id;
 	}
-	public void setId(Longint id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
-	public varchar getRealname() {
+	public String getRealname() {
 		return realname;
 	}
-	public void setRealname(varchar realname) {
+	public void setRealname(String realname) {
 		this.realname = realname;
 	}
-	public varchar getTelephone() {
+	public String getTelephone() {
 		return telephone;
 	}
-	public void setTelephone(varchar telephone) {
+	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-	public varchar getPassword() {
+	public String getPassword() {
 		return password;
 	}
-	public void setPassword(varchar password) {
+	public void setPassword(String password) {
 		this.password = password;
 	}
 	public int getStudent_no() {
@@ -38,16 +39,17 @@ public class User {
 	public void setStudent_no(int student_no) {
 		this.student_no = student_no;
 	}
-	public varchar getGender() {
+	public String getGender() {
 		return gender;
 	}
-	public void setGender(varchar gender) {
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	public bigint getClazz_id() {
+	public Long getClazz_id() {
 		return clazz_id;
 	}
-	public void setClazz_id(bigint clazz_id) {
+	public void setClazz_id(Long clazz_id) {
 		this.clazz_id = clazz_id;
 	}
+	
 }
