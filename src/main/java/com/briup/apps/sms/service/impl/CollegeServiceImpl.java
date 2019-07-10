dfe201ecff3068e32745b5a2488497b090a3484a
朱浩宇 <1214546173@qq.com>
2019-07-10 18:31:44 +0800

    [朱浩宇]修改类

--- src/main/java/com/briup/apps/sms/service/impl/CollegeServiceImpl.java
+++ src/main/java/com/briup/apps/sms/service/impl/CollegeServiceImpl.java
@@ -1,5 +1,38 @@
 package com.briup.apps.sms.service.impl;
 
-public class CollegeServiceImpl {
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.College;
+import com.briup.apps.sms.dao.CollegeDao;
+import com.briup.apps.sms.service.CollegeService;
+
+/**
+ * 大学逻辑逻辑处理的实现类
+ * */
+@Service
+public class CollegeServiceImpl implements CollegeService {
+		// 依赖注入，实例化CollegeDao并且赋值给collegeDao这个变量
+		@Resource
+		private CollegeDao collegeDao;
+		
+		@Override
+		public List<College> selectAll(){
+			return collegeDao.selectAll();
+		}
+		@Override
+		public void saveOrUpdate(College college) throws Exception{
+			if(college.getId()==null) {
+				collegeDao.insert(college);
+			}
+			else {
+				collegeDao.update(college);
+			}
+			
+		}
+		
 
 }
