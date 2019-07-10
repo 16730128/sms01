9c9e0137b7a3fe09e8cdbd89a9ebefcdfe7c2cc4
范东宏 <774784360@qq.com>
2019-07-10 11:41:19 +0800

    [范东宏]填充lDao

--- src/main/java/com/briup/apps/sms/dao/UserDao.java
+++ src/main/java/com/briup/apps/sms/dao/UserDao.java
@@ -1,5 +1,17 @@
 package com.briup.apps.sms.dao;
 
+import java.util.List;
+
+import com.briup.apps.sms.bean.User;
+
+
 public interface UserDao {
+	List<User> selectAll();
+	//查询所有
+	void insert(User user);
+	//插入
+	void update(User user);
+	//更新
+	
 
 }
