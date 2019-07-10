4b28c8584fb371241990a6b30af54ca92e4630ad
冯应琦 <1533180833@qq.com>
2019-07-10 11:40:21 +0800

    [冯应琦]建立了连接

--- src/main/java/com/briup/apps/sms/dao/User_RoleDao.java
+++ src/main/java/com/briup/apps/sms/dao/User_RoleDao.java
@@ -1,5 +1,15 @@
 package com.briup.apps.sms.dao;
 
+import java.util.List;
+
+import com.briup.apps.sms.bean.User_Role;
+
 public interface User_RoleDao {
+   //查询所有
+   List<User_Role> selectAll();
+   void insert(User_Role user_role);
+   //插入
+   void update(User_Role user_role);
+   //更新
    
 }
