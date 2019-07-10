17bb1fe4496811e9cf1bc3c94a0336c9e4ed33bc
冯应琦 <1533180833@qq.com>
2019-07-10 11:44:27 +0800

    [冯应琦]修改了注释

--- src/main/java/com/briup/apps/sms/dao/User_RoleDao.java
+++ src/main/java/com/briup/apps/sms/dao/User_RoleDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.briup.apps.sms.bean.User_Role;
 
 public interface User_RoleDao {
-   //查询所有
+   //查询所有信息
    List<User_Role> selectAll();
    void insert(User_Role user_role);
    //插入
