81f01ade891feec736cb4549acae353a76dbee26
彭浩 <814779307@qq.com>
2019-07-10 18:18:29 +0800

    [彭浩]增加关系类查找方式

--- src/main/java/com/briup/apps/sms/service/RoleService.java
+++ src/main/java/com/briup/apps/sms/service/RoleService.java
@@ -3,7 +3,6 @@ package com.briup.apps.sms.service;
 import java.util.List;
 
 import com.briup.apps.sms.bean.Role;
-import com.briup.apps.sms.bean.Student_Course;
 
 public interface RoleService {
 
--- src/main/java/com/briup/apps/sms/web/controller/RoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/RoleController.java
@@ -1,33 +1,34 @@
 package com.briup.apps.sms.web.controller;
 
-	import java.util.List;
+import java.util.List;
 
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.briup.apps.sms.bean.Role;
-import com.briup.apps.sms.bean.Student_Course;
 import com.briup.apps.sms.service.RoleService;
-import com.briup.apps.sms.service.Student_CourseService;
 
-	//接受参数，返回结果
-
-	@RestController
-	@RequestMapping("/Role")
-	public class RoleController {
-		
-		@Autowired
-		private RoleService roleService;
-		
-		//http://localhost:8080/Role/selectAll
-		@GetMapping("selectAll")
-		
-		public List<Role> selectAll(){
-				return roleService.selectAll();
-				
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+	
+	@Autowired
+	private RoleService roleService;
+	
+	//http://localhost:8080/role/selectAll
+	@GetMapping("selectAll")
+	public List<Role> selectAll(){
+		return roleService.selectAll();
 	}
-	}
-
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
