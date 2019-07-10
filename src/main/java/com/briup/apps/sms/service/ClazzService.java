1b64283ef3778c4925ef5984bd37775ef28bc20e
蒲耀辉 <1025948141@qq.com>
2019-07-10 17:29:01 +0800

    [蒲耀辉]上传查询clazz类

--- src/main/java/com/briup/apps/sms/service/ClazzService.java
+++ src/main/java/com/briup/apps/sms/service/ClazzService.java
@@ -1,5 +1,14 @@
 package com.briup.apps.sms.service;
 
-public interface ClazzService {
+import java.util.List;
 
+import com.briup.apps.sms.bean.Clazz;
+
+//业务逻辑部分
+public interface ClazzService {
+	
+	
+	List<Clazz>  selectAll();
+	
+	void saveOrUpdate(Clazz clazz)  throws Exception;
 }
--- src/main/java/com/briup/apps/sms/web/controller/ClazzController.java
+++ src/main/java/com/briup/apps/sms/web/controller/ClazzController.java
@@ -1,5 +1,28 @@
 package com.briup.apps.sms.web.controller;
 
-public class ClazzController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.Clazz;
+import com.briup.apps.sms.service.ClazzService;
+
+//接受参数，返回结果
+
+@RestController
+@RequestMapping("/Clazz")
+public class ClazzController {
+	
+	@Autowired
+	private ClazzService clazzService;
+	
+	//http://localhost:8080/Clazz/selectAll
+	@GetMapping("selectAll")
+	
+	public List<Clazz> selectAll(){
+			return clazzService.selectAll();
+}
 }
