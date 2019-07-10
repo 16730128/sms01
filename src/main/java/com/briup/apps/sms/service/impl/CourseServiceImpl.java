6f39649a46f1d3a0930c8aa339e08fba8d6c0749
徐晶晶 <393201544@qq.com>
2019-07-10 12:14:29 +0800

    [徐晶晶]创建课程查询类

---
+++ src/main/java/com/briup/apps/sms/service/impl/CourseServiceImpl.java
@@ -0,0 +1,34 @@
+package com.briup.apps.sms.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.Course;
+import com.briup.apps.sms.dao.CourseDao;
+import com.briup.apps.sms.service.CourseService;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+		//依赖注入，实例化CourseDao并且赋值给schoolDao这个变量
+		@Resource
+		private CourseDao courseDao;
+		
+		@Override
+		public List<Course> selectAll(){
+			return courseDao.selectAll();
+		}
+		@Override
+		public void saveOrUpdate(Course course) throws Exception{
+			if(course.getId()==null) {
+				courseDao.insert(course);
+			}
+			else {
+				courseDao.update(course);
+			}
+		}
+		
+
+}
