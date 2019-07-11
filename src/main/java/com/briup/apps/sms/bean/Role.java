b0a2532ec84c7d19cc8bb520e4a1972107280e61
彭浩 <814779307@qq.com>
2019-07-11 14:14:16 +0800

    [彭浩]修改了代码

--- src/main/java/com/briup/apps/sms/bean/Role.java
+++ src/main/java/com/briup/apps/sms/bean/Role.java
@@ -4,6 +4,8 @@ package com.briup.apps.sms.bean;
 public class Role {
 	private Long id;
 	private String name;
+	
+	
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/briup/apps/sms/web/controller/RoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/RoleController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.briup.apps.sms.bean.Role;
-import com.briup.apps.sms.bean.School;
 import com.briup.apps.sms.service.RoleService;
 //接受参数，返回结果
 @RestController
@@ -18,38 +17,43 @@ public class RoleController {
 	
 	@Autowired
 	private RoleService roleService;
-	//http://localhost:8080/role/saveOrUpdate?name="兰州财经大学",description="兰州一所大学",tel="15862568459",address="兰州和平"
-		@PostMapping("saveOrUpdate")
-		public String saveOrUpdate(Role role) {
-			try {
-				roleService.saveOrUpdate(role);
-				return "保存或更新成功";
-			} catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-				return e.getMessage();
-			}
-		}
 	
-	//http://localhost:8080/role/selectAll
-	@GetMapping("selectAll")
-	public List<Role> selectAll(){
-		return roleService.selectAll();
-	}
-	// http://localhost:8080/school/deleteById?id=3
-	@GetMapping("deleteById")
-	public String deleteById(long id) {
+	//http://localhost:8080/role/saveOrUpdate?name="兰州财经大学",description="兰州一所大学",tel="15862568459",address="兰州和平"
+	@PostMapping("saveOrUpdate")
+	public String saveOrUpdate(Role role) {
 		try {
-			roleService.deleteById(id);
-			return "删除成功";
+			roleService.saveOrUpdate(role);
+			return "保存或更新成功";
 		} catch (Exception e) {
-			// 打印异常信息，返回异常信息
+			// TODO: handle exception
 			e.printStackTrace();
 			return e.getMessage();
 		}
 	}
 	
 	
+	//http://localhost:8080/role/selectAll
+	@GetMapping("selectAll")
+	public List<Role> selectAll(){
+		return roleService.selectAll();
+	}
+	
+	
+	//http://localhost:8080/role/deleteById?id=3
+	@GetMapping("deleteById")
+	public String deleteById(long id) {
+		try {
+			roleService.deleteById(id);
+			return "删除成功";
+		}
+		catch(Exception e) {
+			//打印异常信息，返回异常信息
+			e.printStackTrace();
+			return e.getMessage();
+		}
+		
+		
+	}
 	
 	
 	
