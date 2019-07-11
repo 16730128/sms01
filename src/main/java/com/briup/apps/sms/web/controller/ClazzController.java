e56f089fdde5a11bf2d3bd2315f4da05bd0cc10d
蒲耀辉 <1025948141@qq.com>
2019-07-11 13:51:25 +0800

    [蒲耀辉]修改代码

--- src/main/java/com/briup/apps/sms/web/controller/ClazzController.java
+++ src/main/java/com/briup/apps/sms/web/controller/ClazzController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,18 @@ public class ClazzController {
 	
 	@Autowired
 	private ClazzService clazzService;
+	//http://localhost:8080/clazz/saveOrUpdate?name="兰州财经大学",description="兰州一所大学",tel="15862568459",address="兰州和平"
+		@PostMapping("saveOrUpdate")
+		public String saveOrUpdate(Clazz clazz) {
+			try {
+				clazzService.saveOrUpdate(clazz);
+				return "保存或更新成功";
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
 	
 	//http://localhost:8080/Clazz/selectAll
 	@GetMapping("selectAll")
