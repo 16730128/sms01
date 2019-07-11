3bbb24bbf6d99492dda640c97a15298c85f3d523
彭浩 <814779307@qq.com>
2019-07-11 12:35:06 +0800

    [彭浩]上传了一个添加

--- src/main/java/com/briup/apps/sms/web/controller/RoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/RoleController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.briup.apps.sms.bean.Role;
+import com.briup.apps.sms.bean.School;
 import com.briup.apps.sms.service.RoleService;
 //接受参数，返回结果
 @RestController
@@ -16,6 +18,18 @@ public class RoleController {
 	
 	@Autowired
 	private RoleService roleService;
+	//http://localhost:8080/role/saveOrUpdate?name="兰州财经大学",description="兰州一所大学",tel="15862568459",address="兰州和平"
+		@PostMapping("saveOrUpdate")
+		public String saveOrUpdate(Role role) {
+			try {
+				roleService.saveOrUpdate(role);
+				return "保存或更新成功";
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
 	
 	//http://localhost:8080/role/selectAll
 	@GetMapping("selectAll")
