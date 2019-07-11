f2a7d0f8e22507f359a5f105f6bc421da4428bf5
冯应琦 <1533180833@qq.com>
2019-07-11 12:22:05 +0800

    [冯应琦]又更新了代码

--- src/main/java/com/briup/apps/sms/web/controller/User_RoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/User_RoleController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.briup.apps.sms.bean.User_Role;
 import com.briup.apps.sms.service.User_RoleService;
@@ -16,6 +18,18 @@ public class User_RoleController {
 	
 	@Autowired
 	private User_RoleService user_roleService;
+	//http://localhost:8080/user_role/saveOrUpdate?name="兰州财经大学",description="兰州一所大学",tel="15862568459",address="兰州和平"
+		@PostMapping("saveOrUpdate")
+		public String saveOrUpdate(User_Role user_role) {
+			try {
+				user_roleService.saveOrUpdate(user_role);
+				return "保存或更新成功";
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
 	
 	//http://localhost:8080/user_role/selectAll
 	@GetMapping("selectAll")
